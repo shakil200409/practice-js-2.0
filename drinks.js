6b33878e380d9f55b9b0d2a0e41d7d2d053bdbf4
Shakil Ahmed <shakil200409@gmail.com>
2024-09-02 20:41:41 +0600

    drinks for free

--- drinks.js
+++ drinks.js
@@ -1,1 +1,10 @@
-let burger
+let burgerPrice = 600;
+
+if(burgerPrice > 500){
+    let drinksPrice = 0;
+    console.log(drinksPrice);
+}
+else{
+    let drinksPrice = 30;
+    console.log(drinksPrice);
+}
