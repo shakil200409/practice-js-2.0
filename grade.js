b82df05e1df2b1eb5ce27e0cf44df1a77f1b9e62
Shakil Ahmed <shakil200409@gmail.com>
2024-09-02 20:56:53 +0600

    Tried grading system with if else conditions

---
+++ grade.js
@@ -0,0 +1,17 @@
+let mark = 92;
+
+if(mark >= 90 && mark <= 100){
+    console.log("Your grade is: A");
+}
+else if(mark >= 80 && mark < 90){
+    console.log("Your grade is: B");
+}
+else if(mark >= 70 && mark < 80){
+    console.log("Your grade is: C");
+}
+else if(mark >= 60 && mark < 70){
+    console.log("Your grade is: D");
+}
+else{
+    console.log("Your grade is: F");
+}
