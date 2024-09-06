85740833af6eff4d58621ff812ba0d90d164d3b9
Shakil Ahmed <shakil200409@gmail.com>
2024-09-06 20:33:45 +0600

    tried for and while loop

---
+++ for-of.js
@@ -0,0 +1,14 @@
+const numbers = [12, 13, 54, 48, 19, 36, 85, 55, 73];
+
+console.log(numbers);
+
+// for (num of numbers){
+//     console.log(num);
+// }
+
+
+const fruits = ["apple", "mango", "jackfruit", "Watermalon" ];
+for (const fruit of fruits){
+    console.log(fruit);
+    console.log("I will eat.")
+}
